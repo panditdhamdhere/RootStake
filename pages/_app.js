f045332795c2040b69559bfa1acded7dcfa5af7b
panditdhamdhere <dhamdherepandit@gmail.com>
2024-09-03 15:34:00 +0530

    app chain config with wagmi done

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,100 @@
 import "../styles/globals.css";
+import toast, { Toaster } from "react-hot-toast";
+// import merge from "lodash/merge";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+// NETWORK SETUP
+const RootStockTestnetRPC = process.env.NEXT_PUBLIC_ROOTSTOCK_RPC_URL;
+const Explorer = process.env.NEXT_PUBLIC_EXPLORER;
+
+const ChainID = process.env.NEXT_PUBLIC_CHAIN_ID;
+const Currency = process.env.NEXT_PUBLIC_CURRENCY;
+const Decimals = process.env.NEXT_PUBLIC_NETWORK_DECIMALS;
+const Name = process.env.NEXT_PUBLIC_NETWORK_NAME;
+const Network = process.env.NEXT_PUBLIC_NETWORK;
 
 export default function App({ Component, pageProps }) {
+  // NETWORK
+  const { chains, provider } = configureChains(
+    [
+      {
+        id: Number(ChainID),
+        name: Name,
+        network: Network,
+        nativeCurrency: {
+          name: Name,
+          symbol: Currency,
+          decimals: Number(Decimals),
+        },
+        rpcUrls: {
+          default: {
+            http: [`${RootStockTestnetRPC}`],
+          },
+          public: {
+            http: [`${RootStockTestnetRPC}`],
+          },
+        },
+
+        blockExplorers: {
+          default: {
+            name: "RSK explorer",
+            url: Explorer,
+          },
+        },
+        testnet: true,
+      },
+    ],
+    [
+      jsonRpcProvider({
+        rpc: (chain) => {
+          if (chain.id === Number(ChainID)) {
+            return { http: `${RootStockTestnetRPC}` };
+          }
+          return null;
+        },
+        priority: 1,
+      }),
+    ]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "RootStake",
+
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: false,
+    connectors,
+    provider,
+  });
+
+  // const myTheme = merge(midnightTheme(), {
+  //   colors: {
+  //     accentColor: "#562C7B",
+  //     accentColorForeground: "#fff",
+  //   },
+  // });
+
   return (
     <>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} > 
+          {/* // theme={myTheme} */}
+          <Component {...pageProps} />
+          <Toaster />
+        </RainbowKitProvider>
+      </WagmiConfig>
 
       <script src="js/bootstrap.bundle.min.js"></script>
       <script src="js/smooth-scrollbar.js"></script>
